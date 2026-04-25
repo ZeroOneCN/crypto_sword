2936f62e70f63bdad004ed13938fbb34d4c87785
ZeroOneCN <28419158@qq.com>
2026-04-25 14:46:57 +0800

    拆分开平仓执行链路并让主引擎回归调度

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,6 @@
 """Core modules for Hermes Trader runtime."""
 
+from .execution_mixin import ExecutionMixin
 from .models import Position, PositionTracker, TradingConfig
 
-__all__ = ["TradingConfig", "Position", "PositionTracker"]
-
+__all__ = ["TradingConfig", "Position", "PositionTracker", "ExecutionMixin"]
